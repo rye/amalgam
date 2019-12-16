b3a44d7b3d33068e54804fedc7837ec157ffb1bb
Kristofer Rye <kristofer.rye@gmail.com>
2019-12-16 13:36:34 -0600

    Get config loading working
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+// At a high level, read:
+// - Log information (stdin)
+// - Existing configuration (ipset/iptables)
+// - Configuration (file)
+//
+// Ideally, perform actions in streaming mode;
+// configuration defines "safe" hosts and networks.
 fn main() {
-	println!("Hello, world!");
+	let mut settings = config::Config::default();
+	settings
+		.merge(config::File::with_name("config"))
+		.unwrap()
+		.merge(config::Environment::with_prefix("AMALGAM"))
+		.unwrap();
+
+	println!(
+		"{:?}",
+		settings.try_into::<HashMap<String, String>>().unwrap()
+	);
 }
